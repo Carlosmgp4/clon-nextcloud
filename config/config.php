0282d646f666e7044f105f852c54504ce31037bc
carlos <carlosmanuelgamezperezdeguzman@gmail.com>
2022-11-16 11:22:16 +0000

    datos

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ockpqrauoj6g',
+  'passwordsalt' => 'mXgLAJ6yoYd0rr+u0n4Y5JfpEXmZcA',
+  'secret' => '9wIybTeLDpbxcGY6qmNGcCFxjaPtrJ73IKx7Hjklq/VP/WLw',
+  'trusted_domains' => 
+  array (
+    0 => 'cloud.carlos.org',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '25.0.1.1',
+  'overwrite.cli.url' => 'http://cloud.carlos.org',
+  'dbname' => 'nextcloud',
+  'dbhost' => '10.0.0.2',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'nextcloud',
+  'installed' => true,
+);
